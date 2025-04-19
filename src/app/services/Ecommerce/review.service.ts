a6b8cf9c55caaf6101a5ed4f75a83796098c144f
Habosa162 <habosa2000@hotmail.com>
2025-04-19 04:05:22 +0200

    reviewService

--- src/app/services/Ecommerce/review.service.ts
+++ src/app/services/Ecommerce/review.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { baseUrl } from '../enviroment';
+import { Review } from '../../domain/models/Ecommerce/Review.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
+  private apiUrl = `${baseUrl}/Review`
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getReviews(productId:number):Observable<any>{
+      return this.http.get(`${this.apiUrl}/${productId}}`) ;
+  }
+
+  createReview(review : Review) : Observable<any>{
+    return this.http.post(`${this.apiUrl}`, review);
+  }
+  deleteReview(reviewId:number):Observable<any>{
+    return this.http.delete(`${this.apiUrl}/${reviewId}`) ;
+  }
 }
