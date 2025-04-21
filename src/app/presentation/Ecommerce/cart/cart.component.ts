44d658e1b601fa01bbd91f218ff0fc02cffdd2b2
Habosa162 <habosa2000@hotmail.com>
2025-04-21 21:50:17 +0200

    CartComponent

--- src/app/presentation/Ecommerce/cart/cart.component.ts
+++ src/app/presentation/Ecommerce/cart/cart.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../../services/Ecommerce/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  cartItems: any[] = [];
+  totalPrice: number = 0;
+  constructor(private cartService:CartService){}
+  ngOnInit(): void {
+    this.loadCart();
+  }
+  loadCart() {
+    this.cartItems = this.cartService.getCart() ; 
+    this.calculateTotal();
+  }
+  increaseQuantity(productId: number) {
+    this.cartService.updateQuantity(productId, 1);
+    this.loadCart();
+  }
+  decreaseQuantity(productId: number) {
+    this.cartService.updateQuantity(productId,-1);
+    this.loadCart();
+  }
+  removeItem(productId: number) {
+    this.cartService.removeFromCart(productId);
+    this.loadCart();
+  }
+  calculateTotal() {
+    this.totalPrice = this.cartService.getTotalPrice();
+  }
 
+  clearCart() {
+    this.cartService.clearCart();
+    this.loadCart();
+  }
 }
