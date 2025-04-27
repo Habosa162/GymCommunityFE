ba9d61e4ef06a45f5cd7fae6c96fb1bffe9591b1
Habosa162 <habosa2000@hotmail.com>
2025-04-27 11:29:16 +0300

    toaster

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,25 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
-
 import { SocialAuthService, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 import { socialLoginConfig } from './services/social-login.config'; 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(), 
+    provideToastr({
+      positionClass: 'toast-top-right',
+      timeOut: 3000,       
+      preventDuplicates: true,              
+      // closeButton: true,                
+      // progressBar: true,                 
+    }), 
 
     // Social login provider setup
     { provide: 'SocialAuthServiceConfig', useValue: socialLoginConfig },
