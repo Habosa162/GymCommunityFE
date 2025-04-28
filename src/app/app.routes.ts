d75fc13941e1cacaea1dfc124ebf6a3227c86d78
Shawky <shawkygamalcu@gmail.com>
2025-04-28 20:22:30 +0300

    add client plan component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,44 +10,43 @@ import { ResetPasswordComponent } from './presentation/auth/components/reset-pas
 import { ClientProfileComponent } from './presentation/client/components/client-profile/client-profile.component';
 import { TrainingPlansComponent } from './presentation/training-plans/training-plans.component';
 
-import { CoachProfileComponent } from './presentation/Coach/coach-profile/coach-profile.component';
-import { ProductsListComponent } from './presentation/Ecommerce/products-list/products-list.component';
-import { CreateCategoryComponent } from './presentation/Ecommerce/Administration/create-category/create-category.component';
-import { CreateBrandComponent } from './presentation/Ecommerce/Administration/create-brand/create-brand.component';
-import { CreateProductComponent } from './presentation/Ecommerce/Administration/create-product/create-product.component';
-import { WishListComponent } from './presentation/Ecommerce/wish-list/wish-list.component';
+import { AdminCoachGuard } from './core/guards/admin-coach.guard';
+import { AdminGuard } from './core/guards/admin.guard';
 import { AuthGuard } from './core/guards/auth.guard';
 import { clientGuard } from './core/guards/client.guard';
 import { CoachGuard } from './core/guards/coach.guard';
-import { AdminGuard } from './core/guards/admin.guard';
-import { AdminCoachGuard } from './core/guards/admin-coach.guard';
+import { CreateBrandComponent } from './presentation/Ecommerce/Administration/create-brand/create-brand.component';
+import { CreateCategoryComponent } from './presentation/Ecommerce/Administration/create-category/create-category.component';
+import { CreateProductComponent } from './presentation/Ecommerce/Administration/create-product/create-product.component';
 import { ProductDetailsComponent } from './presentation/Ecommerce/product-details/product-details.component';
+import { ProductsListComponent } from './presentation/Ecommerce/products-list/products-list.component';
+import { WishListComponent } from './presentation/Ecommerce/wish-list/wish-list.component';
 
 import { GymOwnerDashboardComponent } from './presentation/Gym/gym-owner-dashboard/gym-owner-dashboard.component';
 
-import { CoachPortfolioComponent } from './presentation/Coach/coach-portfolio/coach-portfolio.component';
 import { CoachCertificatesComponent } from './presentation/Coach/coach-certificates/coach-certificates.component';
-import { CoachWorkSamplesComponent } from './presentation/Coach/coach-work-samples/coach-work-samples.component';
-import { PublicCoachProfileComponent } from './presentation/Coach/public-coach-profile/public-coach-profile.component';
+import { CoachPortfolioComponent } from './presentation/Coach/coach-portfolio/coach-portfolio.component';
 import { CoachRatingComponent } from './presentation/Coach/coach-rating/coach-rating.component';
+import { CoachWorkSamplesComponent } from './presentation/Coach/coach-work-samples/coach-work-samples.component';
 
+import { CoachesListComponent } from './presentation/Coach/coaches-list/coaches-list.component';
+import { CoachViewProfileComponent } from './presentation/Coach/coachviewprofile/coachviewprofile.component';
+import { MyClientsComponent } from './presentation/Coach/my-clients/my-clients/my-clients.component';
 import { CartComponent } from './presentation/Ecommerce/cart/cart.component';
+import { CheckoutComponent } from './presentation/Ecommerce/checkout/checkout.component';
+import { MyOrdersComponent } from './presentation/Ecommerce/my-orders/my-orders.component';
+import { OrderSummaryComponent } from './presentation/Ecommerce/order-summery/order-summary.component';
+import { PaymentSuccessComponent } from './presentation/Ecommerce/payment-success/payment-success.component';
 import { GymDetailsComponent } from './presentation/Gym/gym-details.component/gym-details.component.component';
+import { GymListComponent } from './presentation/Gym/gym-list/gym-list.component';
 import { PlanDetailsComponent } from './presentation/Gym/plan-details/plan-details.component';
 import { SubscriptionDetailsComponent } from './presentation/Gym/subscription-details/subscription-details.component';
-import { CheckoutComponent } from './presentation/Ecommerce/checkout/checkout.component';
-import { CoachesListComponent } from './presentation/Coach/coaches-list/coaches-list.component';
-import { MyClientsComponent } from './presentation/Coach/my-clients/my-clients/my-clients.component';
-import { SelectRoleComponent } from './presentation/auth/components/select-role/select-role.component';
-import { HomeComponent } from './presentation/home/home/home.component';
-import { PaymentSuccessComponent } from './presentation/Ecommerce/payment-success/payment-success.component';
-import { TrainingPlanComponent } from './presentation/traningPlans/training-plan/training-plan.component';
-import { OrderSummaryComponent } from './presentation/Ecommerce/order-summery/order-summary.component';
-import { CoachViewProfileComponent } from './presentation/Coach/coachviewprofile/coachviewprofile.component';
-import { MyOrdersComponent } from './presentation/Ecommerce/my-orders/my-orders.component';
-import { GymListComponent } from './presentation/Gym/gym-list/gym-list.component';
 import { UserGymDetailsComponent } from './presentation/Gym/user-gym-details/user-gym-details.component';
 import { DashboardComponent } from './presentation/admin/dashboard/dashboard.component';
+import { SelectRoleComponent } from './presentation/auth/components/select-role/select-role.component';
+import { HomeComponent } from './presentation/home/home/home.component';
+import { ClientPlansComponent } from './presentation/traningPlans/client-plans/client-plans.component';
+import { TrainingPlanComponent } from './presentation/traningPlans/training-plan/training-plan.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -115,6 +114,11 @@ export const routes: Routes = [
     component: TrainingPlansComponent,
     canActivate: [AuthGuard, AdminCoachGuard],
   },
+  {
+    path: 'client-plans/:id',
+    component: ClientPlansComponent,
+    canActivate: [AuthGuard, clientGuard],
+  },
   //Ecommerce routes
 
   { path: 'shop', component: ProductsListComponent },
@@ -173,9 +177,8 @@ export const routes: Routes = [
     component: SubscriptionDetailsComponent,
   },
 
-  {path: 'gyms/:id', component: UserGymDetailsComponent},
-  {path: 'gyms', component: GymListComponent},
-
+  { path: 'gyms/:id', component: UserGymDetailsComponent },
+  { path: 'gyms', component: GymListComponent },
 
   //coach
 
@@ -215,9 +218,7 @@ export const routes: Routes = [
       {
         path: 'admin',
         canActivate: [AuthGuard, AdminGuard],
-        children: [
-          { path: '', component: DashboardComponent },
-        ],
+        children: [{ path: '', component: DashboardComponent }],
       },
     ],
   },
