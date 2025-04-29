6dc09bae3da976d9c97eec3afe7df60bb690d2d8
Habosa162 <habosa2000@hotmail.com>
2025-04-29 04:18:39 +0300

    adminSideBar

--- src/app/presentation/admin/side-bar/side-bar.component.ts
+++ src/app/presentation/admin/side-bar/side-bar.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+  // Removed 'imports' as it is not valid in the @Component decorator
 @Component({
   selector: 'app-side-bar',
-  imports: [],
+  imports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule,MatButtonModule,RouterModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
