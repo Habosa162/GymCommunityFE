aea7ef179385ffa4b369c56a789310edab7e03f0
Habosa162 <habosa2000@hotmail.com>
2025-05-03 08:36:27 +0300

    usersManagement

--- src/app/services/Admin/admin.service.ts
+++ src/app/services/Admin/admin.service.ts
@@ -17,4 +17,27 @@ export class AdminService {
     const params = { role: role, year: year.toString() };
     return this.http.get(`${this.apiUrl}/users`, { params });
   }
+  
+  getAllUsers(
+    pageNumber: number = 1,
+    pageSize: number = 10,
+    role: string = 'Client',
+    query: string = '',
+    isActive: boolean = true,
+    isPremium: boolean = false,
+    gender: string = 'all'
+  ): Observable<any> {
+    return this.http.get(`${this.apiUrl}/admin/usermanagement`, {
+      params: {
+        role: role,
+        query: query,
+        isActive: isActive.toString(),
+        isPremium: isPremium.toString(),
+        gender: gender,
+        pageNumber: pageNumber.toString(),
+        pageSize: pageSize.toString(),
+      },
+    });
+  }
+  
 }
